ce30cb7184fbb71e844ee1a625b5ee68929d32df
Ola Owolawi <ola.owolawi@xcentium.com>
2017-11-29 00:56:12 -0600

    Updated code with comments

--- Sitecore.Plugin.CustomOrderNumber/Pipelines/Blocks/CustomOrderNumber.cs
+++ Sitecore.Plugin.CustomOrderNumber/Pipelines/Blocks/CustomOrderNumber.cs
@@ -26,6 +26,7 @@ namespace Sitecore.Plugin.CustomOrderNumber.Pipelines.Blocks
         /// <param name="findEntitiesInListCommand"></param>
         public CustomOrderNumber(FindEntitiesInListCommand findEntitiesInListCommand) : base((string)null)
         {
+            // We need to use this to get all existing orders placed before the new order
             _findEntitiesInListCommand = findEntitiesInListCommand;
         }
 
@@ -37,10 +38,13 @@ namespace Sitecore.Plugin.CustomOrderNumber.Pipelines.Blocks
         /// <returns></returns>
         public override Task<Order> Run(Order order, CommercePipelineExecutionContext context)
         {
+            // We need to ensure the order object is not null.
             Condition.Requires<Order>(order).IsNotNull<Order>("The order can not be null");
 
+            // We call a methos that will generate our new order number. We pass parameters that may help in formulating the order number.
             order.OrderConfirmationId = GetCustomOrderNumber(order, context, _findEntitiesInListCommand);
 
+            // return the new order so that it can be sent to the entities databse
             return Task.FromResult<Order>(order);
         }
 
@@ -49,19 +53,29 @@ namespace Sitecore.Plugin.CustomOrderNumber.Pipelines.Blocks
         /// </summary>
         /// <param name="order"></param>
         /// <param name="context"></param>
-        /// <param name="_findEntitiesInListCommand"></param>
+        /// <param name="findEntitiesInListCommand"></param>
         /// <returns></returns>
-        private string GetCustomOrderNumber(Order order, CommercePipelineExecutionContext context, FindEntitiesInListCommand _findEntitiesInListCommand)
+        private string GetCustomOrderNumber(Order order, CommercePipelineExecutionContext context, FindEntitiesInListCommand findEntitiesInListCommand)
         {
-            var orders = (IEnumerable<Order>)_findEntitiesInListCommand.Process<Order>(context.CommerceContext, CommerceEntity.ListName<Order>(), 0, int.MaxValue).Result.Items.ToList<Order>();
+            //Get Contact Component which contains customer information
+            var contactComponent = order.GetComponent<ContactComponent>();
 
+            // get all existing orders.
+            var orders = (IEnumerable<Order>)findEntitiesInListCommand.Process<Order>(context.CommerceContext, CommerceEntity.ListName<Order>(), 0, int.MaxValue).Result.Items;
+
+            // Total orders
+            var orderCount = orders.Count();
+
+            // use the info you have to generate an appropriate order number. You may also use the data you have to call an external system.
+            // in this instance we will just return the number of existing orders incremented by 1
             // Return order count and increment by 1 as the new order number.
             if (orders.Any())
             {
-                return (orders.Count() + 1).ToString();
+                var nextOrderNumber = orderCount + 1;
+                return nextOrderNumber.ToString();
             }
 
-            // return a random guid if ther was an isssue retriving existing orders.
+            // return a random guid if ther was an isssue retriving existing orders or all else failed.
             return Guid.NewGuid().ToString("B");
         }
 
